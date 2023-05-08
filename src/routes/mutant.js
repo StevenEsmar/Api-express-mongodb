1fcff4ab0ff8fe73fd5e027ad73b46a5c639861a
StevenEsmar <stespinosama@unal.edu.co>
2023-05-08 09:14:58 -0500

    get and post request implemented

--- src/routes/mutant.js
+++ src/routes/mutant.js
@@ -12,8 +12,7 @@ router.post('/mutant', function(req, res){
                 (dnaInserted[i][j]==dnaInserted[i][j-3])){
                 counter += 1; 
             } 
-            if( (i == j)&&
-                (i > 2)&&
+            if( (i > 2)&&
                 (dnaInserted[i][j]==dnaInserted[i-1][j-1])&&
                 (dnaInserted[i][j]==dnaInserted[i-2][j-2])&&
                 (dnaInserted[i][j]==dnaInserted[i-3][j-3])){
@@ -25,35 +24,53 @@ router.post('/mutant', function(req, res){
                 (dnaInserted[i][j]==dnaInserted[i-3][j])){
                 counter += 1;
             }
+            if( (i > 2)&&
+                (dnaInserted[i][j]==dnaInserted[i-1][j+1])&&
+                (dnaInserted[i][j]==dnaInserted[i-2][j+2])&&
+                (dnaInserted[i][j]==dnaInserted[i-3][j+3])){
+                counter += 1;
+            }
         }
     }
-
-    if(counter > 1){
-        const person = personModel({
-            dna:"sdfsdfsdfsd",
-            mutant: true 
+    personModel
+    .find()
+    .then((data) => {
+        var dnaExist = false;
+        for (var i in data){
+            if (data.hasOwnProperty(i)) {
+            if(data[i].dna == dnaInserted.toString()){
+                dnaExist = true;
+            }else{
+                dnaExist = false;
             }
-        );
-        
-        person
-            .save()
-            .then((data)=>res.status(200).send("{mutant: true}"))
-            .catch((error)=> res.send(error));    
-        //return res.status(200).send("{mutant: true}");
-
-    }else if (counter <= 1){        
-        const person = personModel({
-            dna:"asdasdasd",
-            mutant: false
             }
-        );
-        
-        person
-            .save()
-            .then((data)=>res.status(403).send("{mutant: false}"))
-            .catch((error)=> res.send(error));    
-        //return res.status(200).send("{mutant: true}");  
-    }
+        };
+        if(dnaExist){
+            res.status(210).send({"message": "dna already exist"})
+        }else{
+            if(counter > 1){
+                const person = personModel({
+                    dna:dnaInserted.toString(),
+                    mutant: true 
+                    }
+                );
+                person
+                    .save()
+                    .then((data)=>res.status(200).send({"mutant": "true"}))
+                    .catch((error)=> res.send(error));    
+            }else if (counter <= 1){        
+                const person = personModel({
+                    dna:dnaInserted.toString(),
+                    mutant: false
+                    }
+                );
+                person
+                    .save()
+                    .then((data)=>res.status(403).send({"mutant": "false"}))
+                    .catch((error)=> res.send(error));    
+            }
+        }
+    });
 });
 
 module.exports = router;
--- src/routes/stats.js
+++ src/routes/stats.js
@@ -1,12 +1,29 @@
 const express = require('express')
 const router = express.Router();
-
-var humans = 400;
-var mutants = 150;
-var ratio = mutants/humans;
+const personModel = require("./../models/person");
 
 router.get('/stats', function(req,res){
-    res.send(`{“count_mutant_dna”:${mutants}, “count_human_dna”:${humans}: “ratio”:${ratio}}`);
+    
+    var humans = 0;
+    var mutants = 0;
+    personModel
+    .find()
+    .then((data) => {
+        for (var i in data){
+            if (data.hasOwnProperty(i)) {
+            if(data[i].mutant == false){
+                humans += 1;
+                }
+            else{
+                mutants += 1;
+            }
+            }
+        };
+        var ratio = mutants/humans;
+        res.json({"ADN":{"count_mutant_dna":mutants, "count_human_dna":humans,"ratio": Number(ratio.toFixed(1))}})
+        }
+    )
+    .catch((error) => res.json({message: error}))
 });
 
 
